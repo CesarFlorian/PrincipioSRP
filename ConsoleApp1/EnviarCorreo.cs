00d35e4874126e91ff5a68a543f3b22f8e4605b5
Fabel Florian De la Cruz <FabelFlorianDelaCruz@hotmail.com>
2024-07-22 10:18:59 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/EnviarCorreo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipioSRP
+{
+    public class EnviarCorreo : Persona
+    {
+        public void EnviarCorreoElectronico(string mensaje) 
+        { 
+           //logica de enviar correo
+        }
+    }
+}
---
+++ ConsoleApp1/ImprimirCorreo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PrincipioSRP
+{
+    public class ImprimirCorreo : EnviarCorreo
+    {
+
+       public void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre:{nombre}");
+            Console.WriteLine($"Nombre:{edad}");
+            Console.WriteLine($"Nombre:{direccion}");
+            Console.WriteLine($"Nombre:{correo}");
+        }
+    }
+}
